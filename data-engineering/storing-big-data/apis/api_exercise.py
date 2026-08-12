0aff399fdf9fcf2a0e01cc2714f3908309c73e22
Nickson Masai <nickso.wafula@gmail.com>
2026-08-12 15:35:54 +0300

    Update API key for AccuWeather API
    Cape Town Data

--- data-engineering/storing-big-data/apis/api_exercise.py
+++ data-engineering/storing-big-data/apis/api_exercise.py
@@ -21,7 +21,7 @@ import urllib.request # <-- Used to call the AccuWeather API.
 
 # =============================================================
 # Fill in your details required to run the script 
-API_KEY = "" # <-- Obtained from your AccuWeather App
+API_KEY = "zpka_19e486a9facd462cbd167af2d4dd10cb_d7e507ef" # <-- Obtained from your AccuWeather App
 LOCATION_ID = "306633" # <-- Obtained from the AccuWeather website
 # =============================================================
 
